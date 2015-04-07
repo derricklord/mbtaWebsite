5597e942f552d67c7c38eaa7bf301d96f74dcd1c
Derrick Lord <derrick.lord@gmail.com>
2015-04-07 12:43:22 -1000

    initial commit

---
+++ client/app.js
@@ -0,0 +1,67 @@
+angular.module('MyApp', ['ngResource', 'ngMessages', 'ui.router', 'mgcrea.ngStrap', 'satellizer'])
+  .config(function($stateProvider, $urlRouterProvider, $authProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'partials/home.html'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'partials/login.html',
+        controller: 'LoginCtrl'
+      })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: 'partials/signup.html',
+        controller: 'SignupCtrl'
+      })
+      .state('logout', {
+        url: '/logout',
+        template: null,
+        controller: 'LogoutCtrl'
+      })
+      .state('/test', {
+        url: '/test',
+        templateUrl: 'partials/data.html',
+        controller: 'AccessCtrl',
+        resolve: {
+          authenticated: function($q, $location, $auth) {
+            var deferred = $q.defer();
+
+            if (!$auth.isAuthenticated()) {
+              $location.path('/login');
+            } else {
+              deferred.resolve();
+            }
+
+            return deferred.promise;
+          }
+        }        
+      })         
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'partials/profile.html',
+        controller: 'ProfileCtrl',
+        resolve: {
+          authenticated: function($q, $location, $auth) {
+            var deferred = $q.defer();
+
+            if (!$auth.isAuthenticated()) {
+              $location.path('/login');
+            } else {
+              deferred.resolve();
+            }
+
+            return deferred.promise;
+          }
+        }
+      });
+
+    $urlRouterProvider.otherwise('/');
+
+
+    $authProvider.google({
+      clientId: '496782544977-q67gsgn3sguk2vcs6hllm5nc33nd4ih1.apps.googleusercontent.com'
+    });
+
+  });
---
+++ client/controllers/access.js
@@ -0,0 +1,31 @@
+angular.module('MyApp')
+  .controller('AccessCtrl', function($scope, $auth, $alert, Account) {
+
+    /**
+     * Get user's profile information.
+     */
+    $scope.getData = function() {
+      Account.getData()
+        .success(function(data) {
+          $scope.data = data.students;
+          $alert({
+            content: 'Student data loaded',
+            animation: 'fadeZoomFadeDown',
+            type: 'material',
+            duration: 3
+          });
+        })
+        .error(function(error) {
+          $alert({
+            content: error.message,
+            animation: 'fadeZoomFadeDown',
+            type: 'material',
+            duration: 3
+          });
+        });
+    };
+
+
+    $scope.getData();
+
+  });
