64f55d29dfd61fe783bcb3fb1d71e3a0e1de9db3
Derrick Lord <derrick.lord@gmail.com>
2015-04-13 10:54:18 -1000

    Refactored app structure

---
+++ routes/users.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../models/users');
+
+
+
+//Show all Authors
+router.get('/', function(req, res){
+	Author.find(function(err, author){
+		res.send(author);
+	});
+}).get('/:id', function(req, res) {
+  Author.findById(req.params.id, function(err, author) {
+    res.send({ book: book });
+  });
+}).post('/', function(req, res) {
+  var author = new Author(req.body);
+  author.save(function(err) {
+    res.send({ author: author });
+  });
+
+}).put('/:id', function(req, res) {
+  Author.findByIdAndUpdate(req.params.id, req.body, function(err, author) {
+    res.send({ author: req.body });
+  });
+}).delete('/:id', function(req, res) {
+  Author.findById(req.params.id).remove(function(err) {
+    res.sendStatus(200);
+  });
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /**
- * Satellizer Node.js Example
- * (c) 2015 Sahat Yalkabov
+ * Myron B. Thompson Academy Website
+ * (c) 2015 Derrick Lord
  * License: MIT
  */
 
+//Load Userland npm Modules
 var path = require('path');
 var qs = require('querystring');
-
 var async = require('async');
 var bcrypt = require('bcryptjs');
 var bodyParser = require('body-parser');
@@ -17,56 +17,31 @@ var moment = require('moment');
 var mongoose = require('mongoose');
 var request = require('request');
 
+//Load Custom Modules (Config/Lib/Models/Routes)
 var config = require('./config');
+var util = require('./lib/util.js');
+var User = require('./models/users'); 
+var testRoutes = require('./routes/test'); 
+var authRoutes = require('./routes/auth');
+var apiRoutes = require('./routes/api');
 
-var userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, lowercase: true },
-  password: { type: String, select: false },
-  displayName: String,
-  picture: String,
-  facebook: String,
-  foursquare: String,
-  google: String,
-  github: String,
-  linkedin: String,
-  live: String,
-  yahoo: String,
-  twitter: String,
-  isAdmin: Boolean
-});
-
-userSchema.pre('save', function(next) {
-  var user = this;
-  if (!user.isModified('password')) {
-    return next();
-  }
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-userSchema.methods.comparePassword = function(password, done) {
-  bcrypt.compare(password, this.password, function(err, isMatch) {
-    done(err, isMatch);
-  });
-};
-
-var User = mongoose.model('User', userSchema);
-
+//Initialize Database
 mongoose.connect(config.MONGO_URI);
 mongoose.connection.on('error', function() {
   console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
 });
 
+
+//Initialize Express
 var app = express();
 
+
+//Configure Server Environment
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'client')));
 
 // Force HTTPS on Heroku
 if (app.get('env') === 'production') {
@@ -75,244 +50,16 @@ if (app.get('env') === 'production') {
     protocol == 'https' ? next() : res.redirect('https://' + req.hostname + req.url);
   });
 }
-app.use(express.static(path.join(__dirname, 'client')));
-
-/*
- |--------------------------------------------------------------------------
- | Login Required Middleware
- |--------------------------------------------------------------------------
- */
-function ensureAuthenticated(req, res, next) {
-  if (!req.headers.authorization) {
-    return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
-  }
-  var token = req.headers.authorization.split(' ')[1];
-  var payload = jwt.decode(token, config.TOKEN_SECRET);
-  if (payload.exp <= moment().unix()) {
-    return res.status(401).send({ message: 'Token has expired' });
-  }
-  req.user = payload.sub;
-  next();
-}
-
-/*
- |--------------------------------------------------------------------------
- | Generate JSON Web Token
- |--------------------------------------------------------------------------
- */
-function createToken(user) {
-  var payload = {
-    sub: user._id,
-    iat: moment().unix(),
-    exp: moment().add(14, 'days').unix()
-  };
-  return jwt.encode(payload, config.TOKEN_SECRET);
-}
-
-/*
- |--------------------------------------------------------------------------
- | GET /test
- |--------------------------------------------------------------------------
- */
-app.get('/test', ensureAuthenticated, function(req, res) {
-
-  var students = [
-    {
-     name: 'John Doe',
-     age: 5,
-     grade: 'K',
-     url: 'http://www.google.com'
-    },
-    {
-     name: 'Jane Doe',
-     age: 5,
-     grade: 'K',
-     url: 'http://www.yahoo.com'
-    },
-    {
-     name: 'Mark Twain',
-     age: 6,
-     grade: '1',
-     url: 'http://www.dogpile.com'
-    }    
-  ];
-  res.send({students:students});
-});
-
-app.get('/users', function(req, res){
-  User.find({}, function(err, users){
-    res.send(users);
-  });
-});
-
-/*
- |--------------------------------------------------------------------------
- | GET /api/me
- |--------------------------------------------------------------------------
- */
-app.get('/api/me', ensureAuthenticated, function(req, res) {
-  User.findById(req.user, function(err, user) {
-    res.send(user);
-  });
-});
-
-/*
- |--------------------------------------------------------------------------
- | PUT /api/me
- |--------------------------------------------------------------------------
- */
-app.put('/api/me', ensureAuthenticated, function(req, res) {
-  User.findById(req.user, function(err, user) {
-    if (!user) {
-      return res.status(400).send({ message: 'User not found' });
-    }
-    console.log(user);
-    user.displayName = req.body.displayName || user.displayName;
-    user.email = req.body.email || user.email;
-    user.save(function(err) {
-      res.status(200).end();
-    });
-  });
-});
 
 
-/*
- |--------------------------------------------------------------------------
- | Log in with Email
- |--------------------------------------------------------------------------
- */
-app.post('/auth/login', function(req, res) {
-  User.findOne({ email: req.body.email }, '+password', function(err, user) {
-    if (!user) {
-      return res.status(401).send({ message: 'Wrong email and/or password' });
-    }
-    user.comparePassword(req.body.password, function(err, isMatch) {
-      if (!isMatch) {
-        return res.status(401).send({ message: 'Wrong email and/or password' });
-      }
-      res.send({ token: createToken(user) });
-    });
-  });
-});
-
-/*
- |--------------------------------------------------------------------------
- | Create Email and Password Account
- |--------------------------------------------------------------------------
- */
-app.post('/auth/signup', function(req, res) {
-  User.findOne({ email: req.body.email }, function(err, existingUser) {
-    if (existingUser) {
-      return res.status(409).send({ message: 'Email is already taken' });
-    }
-    var user = new User({
-      displayName: req.body.displayName,
-      email: req.body.email,
-      password: req.body.password
-    });
-    if(req.body.email === 'derrick.lord@gmail.com'){
-      user.isAdmin = true;
-    }else{
-      user.isAdmin = false;
-    }
-
-
-    user.save(function() {
-      res.send({ token: createToken(user) });
-    });
-  });
-});
-
-/*
- |--------------------------------------------------------------------------
- | Login with Google
- |--------------------------------------------------------------------------
- */
-app.post('/auth/google', function(req, res) {
-  var accessTokenUrl = 'https://accounts.google.com/o/oauth2/token';
-  var peopleApiUrl = 'https://www.googleapis.com/plus/v1/people/me/openIdConnect';
-  var params = {
-    code: req.body.code,
-    client_id: req.body.clientId,
-    client_secret: config.GOOGLE_SECRET,
-    redirect_uri: req.body.redirectUri,
-    grant_type: 'authorization_code'
-  };
-
-  // Step 1. Exchange authorization code for access token.
-  request.post(accessTokenUrl, { json: true, form: params }, function(err, response, token) {
-    var accessToken = token.access_token;
-    var headers = { Authorization: 'Bearer ' + accessToken };
-
-    // Step 2. Retrieve profile information about the current user.
-    request.get({ url: peopleApiUrl, headers: headers, json: true }, function(err, response, profile) {
-
-      // Step 3a. Link user accounts.
-      if (req.headers.authorization) {
-        User.findOne({ google: profile.sub }, function(err, existingUser) {
-          if (existingUser) {
-            return res.status(409).send({ message: 'There is already a Google account that belongs to you' });
-          }
-          var token = req.headers.authorization.split(' ')[1];
-          var payload = jwt.decode(token, config.TOKEN_SECRET);
-          User.findById(payload.sub, function(err, user) {
-            if (!user) {
-              return res.status(400).send({ message: 'User not found' });
-            }
-            user.google = profile.sub;
-            user.picture = user.picture || profile.picture.replace('sz=50', 'sz=200');
-            user.displayName = user.displayName || profile.name;
-            user.save(function() {
-              var token = createToken(user);
-              res.send({ token: token });
-            });
-          });
-        });
-      } else {
-        // Step 3b. Create a new user account or return an existing one.
-        User.findOne({ google: profile.sub }, function(err, existingUser) {
-          if (existingUser) {
-            return res.send({ token: createToken(existingUser) });
-          }
-          var user = new User();
-          user.google = profile.sub;
-          user.picture = profile.picture.replace('sz=50', 'sz=200');
-          user.displayName = profile.name;
-          user.save(function(err) {
-            var token = createToken(user);
-            res.send({ token: token });
-          });
-        });
-      }
-    });
-  });
-});
+//Configure Routes
+app.use('/', testRoutes);
+app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
 
 
 
-/*
- |--------------------------------------------------------------------------
- | Unlink Provider
- |--------------------------------------------------------------------------
- */
-app.get('/auth/unlink/:provider', ensureAuthenticated, function(req, res) {
-  var provider = req.params.provider;
-  User.findById(req.user, function(err, user) {
-    if (!user) {
-      return res.status(400).send({ message: 'User not found' });
-    }
-    user[provider] = undefined;
-    user.save(function() {
-      res.status(200).end();
-    });
-  });
-});
-
-/*
- |--------------------------------------------------------------------------
- | Start the Server
- |--------------------------------------------------------------------------
- */
+//Start Server listening on port 3000
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
